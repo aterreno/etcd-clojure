b6b7ddaf24eef91cf71d9c9393124b882c77515c
Antonio Terreno <antonio.terreno@gmail.com>
2013-10-10 13:35:28 +0100

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject etcd-clojure "0.1.0-SNAPSHOT"
+(defproject etcd-clojure "0.1.0"
   :description "etcd client library in clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/aterreno/etcd-clojure"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:aterreno/etcd-clojure.git"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
                  [clj-http "0.7.7"]])
