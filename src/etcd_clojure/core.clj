33853bc99b2bcbf4eb22fc3aa091841312f1b87e
Antonio Terreno <antonio.terreno@gmail.com>
2013-10-10 11:14:45 +0100

    added naive 'connect' feature

--- src/etcd_clojure/core.clj
+++ src/etcd_clojure/core.clj
@@ -1,14 +1,25 @@
 (ns etcd-clojure.core)
 (:require '[clj-http.client :as client])
 
+(def endpoint (atom "http://127.0.0.1:4001"))
+
+(def api-version "v1")
+
+(defn connect!
+  [etcd-server-uri]
+  (reset! endpoint etcd-server-uri))
+
+(def base-url
+  (str @endpoint "/" api-version))
+
 (defn set
   [key value]
-  (client/put (str "http://127.0.0.1:4001/v1/keys/" key "?value=" value)))
+  (client/put (str base-url "/keys/" key "?value=" value)))
 
 (defn get
   [key]
-  (client/get (str "http://127.0.0.1:4001/v1/keys/" key)))
+  (client/get (str base-url "/keys/" key)))
 
 (defn delete
   [key]
-  (client/delete (str "http://127.0.0.1:4001/v1/keys/" key)))
+  (client/delete (str base-url "/keys/" key)))
