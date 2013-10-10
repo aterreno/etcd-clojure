3ad72f98a7ddb1db9fd7ebb3bd6f52b52216c62d
Antonio Terreno <antonio.terreno@gmail.com>
2013-10-10 13:35:40 +0100

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etcd-clojure "0.1.0"
+(defproject etcd-clojure "0.1.1-SNAPSHOT"
   :description "etcd client library in clojure"
   :url "https://github.com/aterreno/etcd-clojure"
   :lein-release {:deploy-via :clojars}
