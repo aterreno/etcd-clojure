912c62578a7ba8aefaa7a158a8dab87d8cfecacc
Antonio Terreno <antonio.terreno@gmail.com>
2013-10-10 11:59:54 +0100

    added ttl and refactored a bit the building of the url - happy hacking with cond->

--- src/etcd_clojure/core.clj
+++ src/etcd_clojure/core.clj
@@ -1,25 +1,32 @@
 (ns etcd-clojure.core)
 (:require '[clj-http.client :as client])
 
-(def endpoint (atom "http://127.0.0.1:4001"))
+(def ^:private endpoint (atom "http://127.0.0.1:4001"))
 
-(def api-version "v1")
+(def ^:private api-version "v1")
+
+(def ^:private base-url
+  (str @endpoint "/" api-version))
 
 (defn connect!
   [etcd-server-uri]
   (reset! endpoint etcd-server-uri))
 
-(def base-url
-  (str @endpoint "/" api-version))
+(defn- build-url
+  [& {:keys [key value ttl]}]
+  (cond-> base-url
+          key (str "/keys/" key)
+          value (str "?value=" value)
+          ttl (str "?ttl=" ttl)))
 
 (defn set
-  [key value]
-  (client/put (str base-url "/keys/" key "?value=" value)))
+  [key value & {:keys [ttl]}]
+  (client/put (build-url :key key :value value :ttl ttl)))
 
 (defn get
   [key]
-  (client/get (str base-url "/keys/" key)))
+  (client/get (build-url :key key)))
 
 (defn delete
   [key]
-  (client/delete (str base-url "/keys/" key)))
+  (client/delete (build-url :key key)))
