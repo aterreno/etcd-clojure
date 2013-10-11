0b56e9c75158329d277901e4de1e6dbb867f03ed
Antonio Terreno <antonio.terreno@gmail.com>
2013-10-11 14:50:20 +0100

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etcd-clojure "0.1.2-SNAPSHOT"
+(defproject etcd-clojure "0.1.2"
   :description "etcd client library in clojure"
   :url "https://github.com/aterreno/etcd-clojure"
   :lein-release {:deploy-via :clojars}
