d3ced9cde873e4cd5bb8bf6a2206bc4441492d5f
Antonio Terreno <antonio.terreno@gmail.com>
2014-08-18 15:47:12 +0100

    not really happy with the code style but covering more, patterns will emerge

--- src/etcd_clojure/core.clj
+++ src/etcd_clojure/core.clj
@@ -22,14 +22,6 @@
   (reset! endpoint (format "http://%s:4001" etcd-server-host))
   (reset! admin-endpoint (format "http://%s:7001" etcd-server-host)))
 
-(defn- build-url
-  [& {:keys [key value ttl prev-val]}]
-  (cond-> (base-url)
-          key (str "/keys/" key)
-          value (str "?value=" value)
-          prev-val (str "&prevValue=" prev-val)
-          ttl (str "&ttl=" ttl)))
-
 (defn- when-done [future-to-watch function-to-call]
   (future (function-to-call @future-to-watch)))
 
@@ -48,16 +40,46 @@
   (clojure.core/get-in (parse-string (:body (client/get (str (base-url) "/keys/" key)))) ["node" "value"]))
 
 (defn delete
+  "Deletes a value"
   [key]
   (parse-string (:body (client/delete (str (base-url) "/keys/" key)))))
 
 (defn delete-dir
+  "Deletes a dir"
   [key]
   (parse-string (:body (client/delete (str (base-url) "/keys/" key "?dir=true")))))
 
 (defn delete-dir-recur
+  "Deletes a directory recursively"
   [key]
   (parse-string (:body (client/delete (str (base-url) "/keys/" key "?dir=true&recursive=true")))))
+
+(defn create-in-order
+  "Creates in order"
+  [key value]
+  (let [url (str (base-url) "/keys/" key)]
+    (clojure.core/get-in (parse-string (:body (client/post url {:form-params {:value value}}))) ["node" "value"])))
+
+(defn list
+  "Lists the content of a directory"
+  [key & {:keys [recursive]}]
+  (let [url (str (base-url) "/keys/" key "?recursive=" recursive)]
+    (clojure.core/get-in (parse-string (:body (client/get url ))) ["node"])))
+
+(defn list-in-order
+  "Lists the content of a directory recursively and in order"
+  [key]
+  (let [url (str (base-url) "/keys/" key "?recursive=true&sorted=true")]
+    (map #(clojure.core/get % "value") (clojure.core/get-in (parse-string (:body (client/get url ))) ["node" "nodes"]))))
+
+(defn create-dir
+  "Creates a dir"
+  [key & {:keys [ttl]}]
+  (let [data {:value val :dir true}
+        url (str (base-url) "/keys/" key)]
+    (if ttl
+      (clojure.core/get-in (parse-string (:body (client/put url {:form-params (assoc data :ttl ttl)}))) ["node" "key"])
+      (clojure.core/get-in (parse-string (:body (client/put url {:form-params data}))) ["node" "key"]))))
 
 (defn watch
   [key callback]
@@ -65,31 +87,31 @@
     (when-done f #(callback %)) f))
 
 (defn stats
-  ""
+  "Leader stats"
   []
   (parse-string (:body (client/get (str (base-url) "/stats/leader")))))
 
 (defn self-stats
-  ""
+  "Self Stats"
   []
   (parse-string(:body (client/get (str (base-url) "/stats/self")))))
 
 (defn store-stats
-  ""
+  "Store stats"
   []
   (parse-string (:body (client/get (str (base-url) "/stats/store")))))
 
 (defn machines
+  "Machines"
   []
-  (println (str (admin-base-url) api-version "/admin/machines"))
   (parse-string (:body (client/get (str (admin-base-url) "/admin/machines")))))
 
 (defn get-config
-  ""
+  "Gets the config"
   []
   (parse-string (:body (client/get (str (admin-base-url) "/admin/config")))))
 
 (defn get-version
-  ""
+  "Gets the etcd server version"
   []
   (:body (client/get (str @endpoint "/version"))))
