c48e48685c563c2fd9aec7efdc960f546100a538
Antonio Terreno <antonio.terreno@gmail.com>
2014-08-22 14:59:35 +0100

    bump to 0.16 - no backward compatible ! '

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etcd-clojure "0.1.5"
+(defproject etcd-clojure "0.1.6"
   :description "etcd client library in clojure"
   :url "https://github.com/aterreno/etcd-clojure"
   :lein-release {:deploy-via :clojars}
