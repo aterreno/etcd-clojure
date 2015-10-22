1853ac7376cece2f3d29375569df33f68d127152
Antonio Terreno <antonio.terreno@gmail.com>
2015-10-22 06:39:38 +0100

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etcd-clojure "0.2.2-SNAPSHOT"
+(defproject etcd-clojure "0.2.2"
   :description "etcd client library in clojure"
   :url "https://github.com/aterreno/etcd-clojure"
   :lein-release {:deploy-via :clojars}
