28854f3b9737771cf1925c1b83fbd034addba1fa
Antonio Terreno <antonio.terreno@gmail.com>
2016-10-12 15:06:24 +0100

    added :main to use command line lein repl

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Apache license version 2"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :scm {:url "git@github.com:aterreno/etcd-clojure.git"}
+  :main etcd-clojure.core
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.4.0"]
                  [clj-http "1.0.1"]])
